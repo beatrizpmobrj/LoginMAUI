e77577dd636a7f4930ae3a68e58ace645584d396
Beatriz Pinheiro <beatriz.pinheiro@mobrj.com.br>
2022-05-02 09:21:50 -0300

    Definindo a interface IGoogleManager com os métodos de Login e Logout

---
+++ Login/Platforms/Android/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Platforms.Android.ViewModels
+{
+    internal class MainPageViewModel
+    {
+    }
+}
